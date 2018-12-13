c34d8d8cd7965a799edb3862876e58722cefc335
Robert Bartlensky <bartlensky.robert@gmail.com>
2018-12-13 13:28:54 +0000

    Fix instruction encoding and decoding. Only the first 4 bits of an argument were being encoded and decoded. This resulted in instructions such as (LDS, 16, 16, 16) to be encoded and decoded to (LDS, 0, 0, 0).

--- luacompiler/build.rs
+++ luacompiler/build.rs
@@ -2,11 +2,14 @@ extern crate lrlex;
 extern crate lrpar;
 
 use lrlex::LexerBuilder;
+use lrpar::ActionKind;
 use lrpar::CTParserBuilder;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let lex_rule_ids_map =
-        CTParserBuilder::<u8>::new_with_storaget().process_file_in_src("lua5_3/lua5_3.y")?;
+    let mut ct = CTParserBuilder::<u8>::new_with_storaget()
+        .error_on_conflicts(false)
+        .action_kind(ActionKind::GenericParseTree);
+    let lex_rule_ids_map = ct.process_file_in_src("lua5_3/lua5_3.y")?;
     LexerBuilder::new()
         .rule_ids_map(lex_rule_ids_map)
         .process_file_in_src("lua5_3/lua5_3.l")?;
