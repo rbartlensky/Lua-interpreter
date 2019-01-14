0ec46bac5575db2fdceb544d939e9c0564b1b54f
Robert Bartlensky <bartlensky.robert@gmail.com>
2019-01-14 13:42:08 +0000

    Update to grmtools 0.2.

--- luacompiler/build.rs
+++ luacompiler/build.rs
@@ -1,16 +1,17 @@
+extern crate cfgrammar;
 extern crate lrlex;
 extern crate lrpar;
 
+use cfgrammar::yacc::{YaccKind, YaccOriginalActionKind};
 use lrlex::LexerBuilder;
-use lrpar::ActionKind;
 use lrpar::CTParserBuilder;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let mut ct = CTParserBuilder::<u8>::new_with_storaget()
+    let lex_rule_ids_map = CTParserBuilder::<u8>::new_with_storaget()
         .error_on_conflicts(false)
-        .action_kind(ActionKind::GenericParseTree);
-    let lex_rule_ids_map = ct.process_file_in_src("lua5_3/lua5_3.y")?;
-    LexerBuilder::new()
+        .yacckind(YaccKind::Original(YaccOriginalActionKind::GenericParseTree))
+        .process_file_in_src("lua5_3/lua5_3.y")?;
+    LexerBuilder::<u8>::new()
         .rule_ids_map(lex_rule_ids_map)
         .process_file_in_src("lua5_3/lua5_3.l")?;
     Ok(())
