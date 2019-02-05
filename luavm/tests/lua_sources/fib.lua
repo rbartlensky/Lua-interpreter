8c74656df6549e5225f90bee22e4a0ddc1347960
Robert Bartlensky <bartlensky.robert@gmail.com>
2019-02-05 14:29:05 +0000

    Add fibonacci test.

---
+++ luavm/tests/lua_sources/fib.lua
@@ -0,0 +1,14 @@
+function fib(n)
+   if n == 0 then
+      return 0
+   elseif n == 1 then
+      return 1
+   else
+      return fib(n - 1) + fib(n - 2)
+   end
+end
+
+assert(fib(0) == 0)
+assert(fib(1) == 1)
+assert(fib(2) == 1)
+assert(fib(7) == 13)
