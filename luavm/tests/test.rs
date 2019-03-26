f53e35d9ffc1fb4722fa7bd1037af355b849002b
Robert Bartlensky <bartlensky.robert@gmail.com>
2019-03-26 02:23:45 +0000

    Add subset of luajit tests to luavm.

--- luavm/tests/test.rs
+++ luavm/tests/test.rs
@@ -4,17 +4,16 @@ extern crate walkdir;
 
 use luacompiler::{bytecodegen::compile_to_bytecode, irgen::compile_to_ir, LuaParseTree};
 use luavm::Vm;
-
+use std::ops::Add;
+use std::path::Path;
+use std::process::Command;
 use walkdir::WalkDir;
 
 fn compile_and_run(file: &str) {
-    println!("Parsing {}", file);
-    let pt = LuaParseTree::new(file).unwrap();
-    println!("Compiling {}", file);
+    let pt = LuaParseTree::new(file).expect(&format!("Failed to parse {}!", file));
     let bc = compile_to_bytecode(compile_to_ir(&pt));
-    println!("Interpreting {}", file);
     let mut vm = Vm::new(bc, vec![]);
-    vm.eval().unwrap();
+    vm.eval().expect(&format!("Failed to execute {}!", file));
 }
 
 #[test]
@@ -27,5 +26,33 @@ fn test_lua_sources() {
         })
     {
         compile_and_run(entry.unwrap().path().to_str().unwrap())
+    }
+}
+
+const LUAJIT_DIR: &'static str = "./tests/luajit2-test-suite/";
+const LUAJIT_TESTS: [&'static str; 6] = [
+    "ack.lua",
+    "ack_notail.lua",
+    "assign_tset_prevnil.lua",
+    "assign_tset_tmp.lua",
+    "tak.lua",
+    "strcmp.lua",
+];
+
+#[test]
+fn test_luajit_suite() {
+    // test if luajit-test-suite is cloned
+    if !Path::new(LUAJIT_DIR).exists() {
+        // clone the repo
+        Command::new("git")
+            .arg("clone")
+            .arg("https://github.com/openresty/luajit2-test-suite")
+            .arg(LUAJIT_DIR)
+            .status()
+            .expect("'git clone' command failed to start");
+    }
+    for test in LUAJIT_TESTS.iter() {
+        let source_file = String::from(LUAJIT_DIR).add("test/misc/").add(test);
+        compile_and_run(&source_file);
     }
 }
